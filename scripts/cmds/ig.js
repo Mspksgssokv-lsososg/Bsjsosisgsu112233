0d9612f91031aebc3636f3f0a8ff293e7fdaacc2
Mspksgssokv-lsososg <itssiddik487@gmail.com>
2026-03-30 14:02:43 +0600

    Update ig.js

--- scripts/cmds/ig.js
+++ scripts/cmds/ig.js
@@ -1,37 +1,36 @@
 module.exports = {
-  config: {
-    name: "ig",
-    aliases: ["/"], // 🔥 / দিয়ে trigger হবে
-    version: "1.0.0",
-    author: "RAKIB MAHMUD (fixed)",
-    description: "Random Islamic caption + photo",
-    category: "fun",
-    usePrefix: false // prefix লাগবে না
-  },
+    config: {
+        name: "ig",
+        aliases: ["/"], // / দিয়ে trigger হবে
+        version: "1.0.0",
+        author: "RAKIB MAHMUD (fixed)",
+        description: "Random Islamic caption + photo",
+        category: "fun",
+        usePrefix: false
+    },
 
-  onStart: async function ({ bot, msg, chatId }) {
-    try {
-      const captions = [
-        "ღ••\n– কোনো নেতার পিছনে নয়.!!🤸‍♂️\n– মসজিদের ইমামের পিছনে দাড়াও জীবন বদলে যাবে ইনশাআল্লাহ.!!🖤🌻\n۵",
-        "-!\n__আল্লাহর রহমত থেকে নিরাশ হওয়া যাবে না!” আল্লাহ অবশ্যই তোমাকে ক্ষমা করে দিবেন☺️🌻\nসুরা যুমাহ্ আয়াত ৫২..৫৩💙🌸\n-!",
-        "- ইসলাম অহংকার করতে শেখায় না!🌸\n- ইসলাম শুকরিয়া আদায় করতে শেখায়!🤲🕋🥀"
-      ];
+    onStart: async function ({ bot, msg, chatId }) {
+        try {
+            const captions = [
+                "ღ••\n– কোনো নেতার পিছনে নয়.!!🤸‍♂️\n– মসজিদের ইমামের পিছনে দাড়াও জীবন বদলে যাবে ইনশাআল্লাহ.!!🖤🌻\n۵",
+                "-!\n__আল্লাহর রহমত থেকে নিরাশ হওয়া যাবে না! আল্লাহ অবশ্যই তোমাকে ক্ষমা করে দিবেন☺️🌻\nসুরা যুমাহ্ আয়াত ৫২..৫৩💙🌸\n-!",
+                "- ইসলাম অহংকার করতে শেখায় না!🌸\n- ইসলাম শুকরিয়া আদায় করতে শেখায়!🤲🕋🥀"
+            ];
 
-      const links = [
-        "https://i.postimg.cc/7LdGnyjQ/images-31.jpg",
-        "https://i.postimg.cc/65c81ZDZ/images-30.jpg",
-        "https://i.postimg.cc/Y0wvTzr6/images-29.jpg"
-      ];
+            const links = [
+                "https://i.postimg.cc/7LdGnyjQ/images-31.jpg",
+                "https://i.postimg.cc/65c81ZDZ/images-30.jpg",
+                "https://i.postimg.cc/Y0wvTzr6/images-29.jpg"
+            ];
 
-      const randomCaption = captions[Math.floor(Math.random() * captions.length)];
-      const randomImageLink = links[Math.floor(Math.random() * links.length)];
+            const randomCaption = captions[Math.floor(Math.random() * captions.length)];
+            const randomImageLink = links[Math.floor(Math.random() * links.length)];
 
-      await bot.sendPhoto(chatId, randomImageLink, {
-        caption: randomCaption
-      });
+            // 🔥 botInstance থেকে chatId দিয়ে photo পাঠানো
+            await bot.sendPhoto(chatId, randomImageLink, { caption: randomCaption });
 
-    } catch (error) {
-      console.error("Error in ig command:", error);
+        } catch (error) {
+            console.error("Error in ig.js:", error);
+        }
     }
-  }
 };
